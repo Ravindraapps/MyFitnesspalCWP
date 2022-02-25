cb218d652ba6a3f55c1f3e8ce0c4aa8bbe1ff3f6
Nikhil Tiwari <93372211+NikhilTiwari29@users.noreply.github.com>
2022-02-25 16:52:35 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './home';
+import NewPage from "./newPage";
+
+function App() {
+  return (
+    <div className="App">
+      <NewPage />
+    </div>
+  );
+}
+
+export default App;
